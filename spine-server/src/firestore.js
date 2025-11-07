c2803f0a1f5191ed9bb3d7aeacac20bc489a9996
LAPTOP-CQKMPKD8\ASUS <lou.lou@kangdainfo.com>
2025-11-07 14:12:54 +0800

    [fix] 頸椎分析 首次建立 無法新增商品, 因 firestore 之 storageBucket 須設定。

--- spine-server/src/firestore.js
+++ spine-server/src/firestore.js
@@ -4,7 +4,7 @@ var serviceAccount = require("./firestroekey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "gs://newshopbackup2.firebasestorage.app" //  open  backup2 newshop Firebase Storage Bucket
+  storageBucket: "gs://spineanalysis1006test1.firebasestorage.app" //  open  backup2 newshop Firebase Storage Bucket
 });
 
 const db = admin.firestore();
